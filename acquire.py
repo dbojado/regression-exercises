c45a7a2d3ff884de1b912521ffdf28e4006f158a
Daniella Bojado <daniella.bojado@gmail.com>
2020-09-24 11:43:32 -0500

    add acquire python file

--- acquire.py
+++ acquire.py
@@ -3,6 +3,38 @@ import numpy as np
 import os
 from env import host, username, password
 
+
+###################### Acquire Mall Customers Data #####################
+
+def get_connection(db, username=username, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{username}:{password}@{host}/{db}'
+
+def new_mall_data():
+    '''
+    This function reads the mall customer data from the Codeup db into a df, 
+    write it to a csv file, and returns the df.
+    '''
+    sql_query =  'SELECT * FROM customers'
+    df = pd.read_sql(sql_query, get_connection('mall_customers'))
+    df.to_csv('mall_customers_df.csv')
+    return df
+
+def get_mall_data(cached=False):
+    '''
+    This function reads in mall customer data from Codeup database if cached == False
+    or if cached == True reads in mall customer df from a csv file, returns df
+    '''
+    if cached or os.path.isfile('mall_customers_df.csv') == False:
+        df = new_mall_data()
+    else:
+        df = pd.read_csv('mall_customers_df.csv', index_col=0)
+    return df
+
+
 ###################### Acquire Titanic Data ######################
 
 def get_connection(db, user=username, host=host, password=password):
